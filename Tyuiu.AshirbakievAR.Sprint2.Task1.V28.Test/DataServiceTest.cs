ea11d52d48eed618e2b04ad1e9db722ad67a83ec
Tyuiu.AshirbakievAR@outlook.com <almazashirbakiev@gmail.com>
2023-10-12 21:21:17 +0500

    Task1

---
+++ Tyuiu.AshirbakievAR.Sprint2.Task1.V28.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AshirbakievAR.Sprint2.Task1.V28.Lib;
+
+namespace Tyuiu.AshirbakievAR.Sprint2.Task1.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void GetLogicOperationsTest()
+        {
+            DataService ds = new DataService();
+            bool[] wait = new bool[6] { true, false, true, false, true, false };
+            int a = 247, b = 654, c = 671, d = 671;
+            CollectionAssert.AreEqual(wait, ds.GetLogicOperations(a, b, c, d));
+        }
+    }
+}
---
+++ Tyuiu.AshirbakievAR.Sprint2.Task1.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AshirbakievAR.Sprint2.Task1.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AshirbakievAR.Sprint2.Task1.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("03db2171-6ec0-4b36-8933-2fed4d90b7a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
