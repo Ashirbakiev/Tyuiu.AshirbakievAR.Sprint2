a4187ceb1dba75b1871bb2f8fbe45588037642b3
Tyuiu.AshirbakievAR@outlook.com <almazashirbakiev@gmail.com>
2023-10-14 14:38:52 +0500

    Task7

---
+++ Tyuiu.AshirbakievAR.Sprint2.Task7.V13/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AshirbakievAR.Sprint2.Task7.V13.Lib;
+
+namespace Tyuiu.AshirbakievAR.Sprint2.Task7.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Аширбакиев А. Р. | СмартБ 23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Аширбакиев Алмаз Ринатович | СмартБ 23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на C#, которая запрашивает исходные                  *");
+            Console.WriteLine("* данные (вещественные значения) и вычисляет, находится ли точка с        *");
+            Console.WriteLine("* координатами X,Y в заштрихованной области.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите значение переменной X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if (ds.CheckDotInShadedArea(x, y)) Console.WriteLine("Точка находится в заштрихованной области");
+            else Console.WriteLine("Точка не находится в заштрихованной области");
+
+            Console.ReadKey();
+        }
+    }
+}
